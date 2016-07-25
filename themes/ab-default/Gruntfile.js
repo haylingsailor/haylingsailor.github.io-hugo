e5708db4e14426871e984c00b62855290acce7ea
Andy Ballingall <andy.ballingall@gmail.com>
2016-07-25 22:00:02 +0100

    Got sass compilation working

---
+++ themes/ab-default/Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        options: {
+          sourcemap: false,
+          compress: false,
+          yuicompress: false,
+          style: 'expanded',
+        },
+        files: {
+          'static/css/main.css' : 'src/sass/main.scss'
+        }
+      },
+    },
+    postcss: {
+      options: {
+        map: true,
+        processors: [
+          require('pixrem')(), // add fallbacks for rem units
+          require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+          require('cssnano')() // minify the result
+        ]
+      },
+      dist: {
+        src: 'static/css/*.css'
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass', 'postcss']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.registerTask('default',['watch']);
+};
